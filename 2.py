cbb4dd04bfedc250602e20ef9b18737bafe9c28b
jiang-rz <1183557197@qq.com>
2022-02-18 09:58:40 +0800

    talib导包

--- 2.py
+++ 2.py
@@ -1,6 +1,8 @@
+#talib
 import pandas as pd
 import talib as ta
 gold=pd.read_csv(r'C:\Users\Lenovo\Desktop\2022_Problem_C_DATA\gold.csv')
+
 gold.columns=['1','Date','data']
 gold=gold.drop(['1'],axis=1)
 
